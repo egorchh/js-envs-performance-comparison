7db11164a117c6c8ddad6ef411656ec5b9791d1f
Egor Podolskiy <eapodolskiy@avito.ru>
2025-03-20 19:19:11 +0300

    add deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/js-envs-performance-comparison/',
   server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:3000',
+                target: process.env.VITE_API_URL || 'http://localhost:3000',
                 changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
             }
         }
     },
