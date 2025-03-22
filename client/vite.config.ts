cff8a08d4f2fe8a6d00fb5ee4352fe204e5e3083
Egor Podolskiy <eapodolskiy@avito.ru>
2025-03-22 15:52:54 +0300

    fix vite default port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,17 +4,18 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [react()],
-    base: '/js-envs-performance-comparison/',
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL || 'http://localhost:5555',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        }
-      }
-    },
-  }
+    return {
+            plugins: [react()],
+            base: '/js-envs-performance-comparison/',
+            server: {
+                port: 3000,
+                proxy: {
+                    '/api': {
+                    target: env.VITE_API_URL || 'http://localhost:5555',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                    }
+                }
+            },
+    }
 })
